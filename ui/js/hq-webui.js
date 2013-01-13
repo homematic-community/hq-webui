3860dc258ddbdbb0e292149c996c1e01660b777b
hobbyquaker <hobbyquaker@gmail.com>
2013-01-13 21:23:44 +0100

    2.1-alpha10 - see Readme

--- ui/js/hq-webui.js
+++ ui/js/hq-webui.js
@@ -22,7 +22,7 @@ jQuery.extend(jQuery.expr[ ":" ], {
 
 (function ($) { $("document").ready(function () {
 
-    var version =               "2.1-beta1";
+    var version =               "2.1-alpha10";
 
     var statesXML,
         rssiXML,
